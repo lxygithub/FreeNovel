8aa74fcdfac88ee84f45a289ddfdfd26b1b84e93
lxygithub <594328762@qq.com>
2020-02-20 14:04:59 +0800

    记录搜索历史

--- app/src/main/java/cn/mewlxy/novel/ui/SearchActivity.kt
+++ app/src/main/java/cn/mewlxy/novel/ui/SearchActivity.kt
@@ -1,6 +1,8 @@
 package cn.mewlxy.novel.ui
 
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
@@ -15,14 +17,17 @@ import cn.mewlxy.novel.jsoup.OnJSoupListener
 import cn.mewlxy.novel.listener.RVOScrollListener
 import cn.mewlxy.novel.model.BookModel
 import cn.mewlxy.novel.utils.showToast
+import com.mewlxy.readlib.utlis.SpUtil
 import kotlinx.android.synthetic.main.activity_search.*
+import kotlinx.android.synthetic.main.layout_search_history.*
+import me.next.tagview.TagCloudView
 import org.jsoup.nodes.Document
 
 /**
  * description：
  * author：luoxingyuan
  */
-class SearchActivity : BaseActivity(), View.OnClickListener, TextView.OnEditorActionListener {
+class SearchActivity : BaseActivity(), View.OnClickListener, TextView.OnEditorActionListener, TagCloudView.OnTagClickListener, TextWatcher {
     private val domSoup = DomSoup()
     private var bookModels = ArrayList<BookModel>()
     private lateinit var bookListAdapter: BookListAdapter
@@ -30,6 +35,8 @@ class SearchActivity : BaseActivity(), View.OnClickListener, TextView.OnEditorAc
     private var totalPage = 1
     private var isLoading = false
     private var keyword = ""
+    private var searchHistory = ""
+    private lateinit var tags: List<String>
     override fun getLayoutResId(): Int {
         return R.layout.activity_search
     }
@@ -38,7 +45,7 @@ class SearchActivity : BaseActivity(), View.OnClickListener, TextView.OnEditorAc
         rv_search_result.layoutManager = LinearLayoutManager(this)
         et_search.setOnEditorActionListener(this)
         iv_back.setOnClickListener(this)
-
+        tag_cloud_view.setOnTagClickListener(this)
     }
 
     override fun initData() {
@@ -63,6 +70,17 @@ class SearchActivity : BaseActivity(), View.OnClickListener, TextView.OnEditorAc
             }
 
         })
+        updateSearchHistory()
+
+        et_search.addTextChangedListener(this)
+    }
+
+    private fun updateSearchHistory() {
+        searchHistory = SpUtil.getStringValue("search_history", "")
+        tags = searchHistory.trimEnd(',').split(",").toMutableList()
+        if (searchHistory.isNotBlank()) {
+            tag_cloud_view.setTags(tags)
+        }
     }
 
     override fun onClick(v: View?) {
@@ -75,6 +93,7 @@ class SearchActivity : BaseActivity(), View.OnClickListener, TextView.OnEditorAc
 
     //搜索
     private fun search(keyword: String, page: Int) {
+
         domSoup.getSoup(Source.QUANBEN + "/index.php?c=xs&a=search&keywords=${keyword}&page=${page}", object : OnJSoupListener {
             override fun start() {
                 isLoading = true
@@ -109,6 +128,12 @@ class SearchActivity : BaseActivity(), View.OnClickListener, TextView.OnEditorAc
                 showToast(errMsg)
             }
         })
+
+        if (searchHistory != null && !searchHistory!!.contains(keyword)) {
+            searchHistory += "${keyword},"
+            SpUtil.setStringValue("search_history", searchHistory)
+            updateSearchHistory()
+        }
     }
 
     //点击搜索
@@ -122,6 +147,28 @@ class SearchActivity : BaseActivity(), View.OnClickListener, TextView.OnEditorAc
             }
         }
         return true
+    }
+
+    override fun onTagClick(position: Int) {
+        et_search.setText(tags[position])
+        currentPage = 1
+        search(tags[position], currentPage)
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        if (s == null || s.isBlank()) {
+            view_search_history.visibility = View.VISIBLE
+            rv_search_result.visibility = View.GONE
+        } else {
+            view_search_history.visibility = View.GONE
+            rv_search_result.visibility = View.VISIBLE
+        }
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
     }
 
 }
